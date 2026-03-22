aec250f0a62e10239e60628a1c78f9a1a67efd1d
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2026-03-21 23:53:48 -0400

    criação do componente input e criação da tela de cadastro(SignUP)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import {SignUpScreen} from './screens/authentication/signUp'
 
 export function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <SignUpScreen/>
       <StatusBar style="auto" />
     </View>
   );
