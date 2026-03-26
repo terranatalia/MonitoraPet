d94a9d515030dea81de9515a67dc6db9be21c92c
Arthur Macedo F. F. <146777649+arthurmacedoo@users.noreply.github.com>
2026-03-26 10:07:01 -0400

    Add styles for reset password screen

---
+++ frontend/src/screens/authentication/resetPassword/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 500,
+        height: 300,
+    },
+    voltarLogin: {
+        fontSize: 20,
+        alignSelf: 'center',
+        fontWeight: '600',
+        marginTop: 15,
+    },
+    LoginLink: {
+        color: '#d47c2f',
+    },
+    button: {
+        backgroundColor: '#2e6693',
+        width: 300,
+        alignSelf: 'center',
+        marginTop: 18,
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 10,
+        elevation: 6,
+        shadowColor: 'black',
+    },
+    buttonLabel: {
+        fontSize: 22,
+        color: '#fff'
+    }
+});
